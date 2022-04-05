4f7338d7d315973004ae45a32d76e9b9796ebf9c
ggombee <eunbko@actbase.io>
2022-04-06 04:55:03 +0900

    edit kakao message

--- src/pages/Result.js
+++ src/pages/Result.js
@@ -33,7 +33,7 @@ const Result = () => {
           <Button style={{ fontFamily: "SimKyungha", width: 170 }} onClick={() => navigate("/")}>
             테스트 다시하기
           </Button>
-          <KakaoShareButton />
+          <KakaoShareButton data={resultData} />
         </ButtonGroup>
       </Contents>
     </Wrapper >
